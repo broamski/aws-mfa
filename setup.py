135a264036d0d25b1e67241ae21561824cbeaac7
broamski <brian.nuszkowski@me.com>
2015-12-21 19:28:13 -0500

    Version bump to reflect most recent changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aws-mfa',
-    version='0.0.3',
+    version='0.0.4',
     description='Manage AWS MFA Security Credentials',
     author='Brian Nuszkowski',
     author_email='nuszkowski@protonmail.com',
