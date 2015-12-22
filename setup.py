1e128ceefcb5667ea8bc716c734137d52ed140c5
broamski <brian.nuszkowski@me.com>
2015-12-21 19:02:21 -0500

    Version bump after README fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aws-mfa',
-    version='0.0.1',
+    version='0.0.2',
     description='Manage AWS MFA Credentials',
     author='Brian Nuszkowski',
     scripts=['aws-mfa'],
