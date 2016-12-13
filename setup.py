c3e4e4cd1f980f84fa075f16263e2b68bba32d9c
broamski <brian.nuszkowski@me.com>
2016-12-13 00:04:15 -0500

    bumping version to 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aws-mfa',
-    version='0.0.7',
+    version='0.0.8',
     description='Manage AWS MFA Security Credentials',
     author='Brian Nuszkowski',
     author_email='nuszkowski@protonmail.com',
