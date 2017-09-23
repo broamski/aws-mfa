51843e06dcd8682b91dd957f4d3aa4f8d35e6c0b
broamski <brian.nuszkowski@me.com>
2017-09-22 22:34:40 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='aws-mfa',
-    version='0.0.8',
+    version='0.0.9',
     description='Manage AWS MFA Security Credentials',
     author='Brian Nuszkowski',
-    author_email='nuszkowski@protonmail.com',
+    author_email='brian@bnuz.co',
     scripts=['aws-mfa'],
     url='https://github.com/broamski/aws-mfa',
     install_requires=['boto3>=1.2.3']
